0a50643e2f7492dbee68a65dee491150a0323d12
sandypockets <zac@znacreative.com>
2021-04-12 15:22:56 -0400

    Button testing in Storybook working

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,12 +11,14 @@ export default function Button(props) {
   if (props.danger) {
     buttonClass += " button--danger"
   }
-  if (props.clickable) {
-    buttonClass += " button--clickable"
-  }
-  if (props.disabled) {
-    buttonClass += " button--disabled"
-  }
 
-  return <button className={buttonClass}>{props.children}</button>;
+  return (
+    <button 
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
