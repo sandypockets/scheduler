3a667fe0a027c6679f080371e07842be723dc265
sandypockets <zac@znacreative.com>
2021-04-12 15:09:31 -0400

    Storybook testing for Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,20 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm"
+  }
+  if (props.danger) {
+    buttonClass += " button--danger"
+  }
+  if (props.clickable) {
+    buttonClass += " button--clickable"
+  }
+  if (props.disabled) {
+    buttonClass += " button--disabled"
+  }
+
+  return <button className={buttonClass}>{props.children}</button>;
 }
