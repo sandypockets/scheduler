8c865295f8ef982fd5f2db0ae93d6b31af77c0ee
sandypockets <zac@znacreative.com>
2021-04-12 18:31:08 -0400

    Add classNames and import .scss

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,19 @@
 import React from "react";
+import classNames from 'classnames';
+
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayClass = classNames(
+    {'day-list__item': true},
+    {'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots
+  });
+
   return (
     <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h2 className={dayClass}>{props.name}</h2> 
+      <h3 className={dayClass}>{props.spots}</h3>
     </li>
   );
 }
