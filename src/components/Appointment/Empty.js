f31764df914f56e99cc50118bf64298bf937b133
sandypockets <zac@znacreative.com>
2021-04-13 19:17:41 -0400

    Create component for Empty

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -8,7 +8,6 @@ export default function Empty(props) {
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
-        // Need to add onAdd:Function to be called when the user clicks the Add button
         onClick={props.onAdd}
       />
     </main>
