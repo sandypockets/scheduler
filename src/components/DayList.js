7e0af7cc5b8a1fc30ce5c22f44f13efb1d3df8fe
sandypockets <zac@znacreative.com>
2021-04-14 17:45:57 -0400

    Fix setDay props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,19 +2,22 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
+  console.log("DayList 5 - ", props)
   const { days } = props;
+  console.log("DayList 7 - ", days);
 
   const day = days.map((days, index) => {
+    console.log("DayList 9 - ", days)
     return (
         <DayListItem
         key={index}
         name={days.name}
         spots={days.spots}
         selected={days.name === props.day}
-        setDay={props.setDay} 
+        setDay={props.setDay}
       />
     )
-  })
+  });
 
   return (
     <ul>
