70c63e7df0210670c8c227b60ca3214a4627472e
sandypockets <zac@znacreative.com>
2021-04-13 20:37:48 -0400

    Add Form testing to Storybook

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -159,3 +160,5 @@ storiesOf("Button", module)
           .add("Status", () => <Status message={"Deleting..."} />)
           .add("Error", () => <Error message={"Could not delete appointment."} />)
           .add("Error on close", () => <Error message={"Could not delete appointment."} onClose={action("onClose")} />)
+          .add("Form on edit", () => <Form name={'Bob'} interviewers={interviewers} interviewer={1} onSave={action("onSave")} onCancel={action("onCancel")} />)
+          .add("Form on create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
