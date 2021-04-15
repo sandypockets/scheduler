782cc6edc0d8dfac284f5dec9e7a129dd4f4bfd2
sandypockets <zac@znacreative.com>
2021-04-15 13:40:31 -0400

    Remove unnecessary console.logs from debugging

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,12 +2,8 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
-  console.log("DayList 5 - ", props)
   const { days } = props;
-  console.log("DayList 7 - ", days);
-
   const day = days.map((days, index) => {
-    console.log("DayList 9 - ", days)
     return (
         <DayListItem
         key={index}
