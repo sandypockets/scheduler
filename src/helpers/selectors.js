64a315c24516c1478dfe4895cb84837ca8a161b6
sandypockets <zac@znacreative.com>
2021-04-15 13:41:00 -0400

    Create func to help filter data into useful format

---
+++ src/helpers/selectors.js
@@ -0,0 +1,7 @@
+export default function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.find(elem => day === elem.name);
+  if (state.days.length === 0 || filteredDays === undefined) {
+    return [];
+  }
+  return filteredDays.appointments.map(id => state.appointments[id]);
+}
