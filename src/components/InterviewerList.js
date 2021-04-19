87975c0718dc48ba3084854a698c39b2c929a987
sandypockets <zac@znacreative.com>
2021-04-19 13:33:27 -0400

    Add PropTypes checking to InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
@@ -16,7 +17,7 @@ export default function InterviewerList(props) {
       />
     )
   })
-
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
@@ -24,3 +25,7 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
