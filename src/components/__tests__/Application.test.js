1510d3cd5ffcb858e00915a39ee68dfd1df0355f
sandypockets <zac@znacreative.com>
2021-04-21 18:17:06 -0400

    Remove commented console.log

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -8,7 +8,6 @@ afterEach(cleanup);
 it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
   const { container } = render(<Application />);
   await waitForElement(() => getByText(container, "Archie Cohen"));
-  //console.log(prettyDOM(container));
 });
 
 it("shows the save error when failing to save an appointment", () => {
