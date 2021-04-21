2fa23fc4f79bf00ce8be134305732e8ced9c6499
sandypockets <zac@znacreative.com>
2021-04-21 16:12:29 -0400

    Create and pass Cancel Interview test

--- cypress/integration/appointment.spec.js
+++ cypress/integration/appointment.spec.js
@@ -3,7 +3,7 @@ describe("Booking Actions", () => {
     // Visits the root of our web server
     cy.visit('/')
     // Clicks on the "Add" button in the second appointment
-    cy.get(':nth-child(3) > .appointment__add > [data-testid=add]').click()
+      .get(':nth-child(3) > .appointment__add > [data-testid=add]').click()
       .get('[data-testid=student-name-input]')
     // Enters their name
       .type('Bob Test Name')
@@ -19,7 +19,7 @@ describe("Booking Actions", () => {
     // Visits the root of our web server
     cy.visit('/')
     // Clicks the edit button for the existing appointment
-    cy.get('[alt=Edit]').first().click({ force: true })
+      .get('[alt=Edit]').first().click({ force: true })
     // Changes the name and interviewer
       .get('[data-testid=student-name-input]')
       .clear()
@@ -28,14 +28,18 @@ describe("Booking Actions", () => {
     // Clicks the save button
       .get('.button--confirm').click()
     // Sees the edit to the appointment
-    .get(':nth-child(1) > .appointment__card')
+      .get(':nth-child(1) > .appointment__card')
   })
 
   it("should cancel an interview", () => {
-
     // Visits the root of our web server
+    cy.visit('/')
     // Clicks the delete button for the existing appointment
+      .get(':nth-child(2) > .appointment__card')
+      .get('[alt=Delete]').first().click({ force: true })
     // Clicks the confirm button
+      .get('.appointment__card > .appointment__actions > :nth-child(2)').click()
     // Sees that the appointment slot is empty
+      .get(':nth-child(1) > .appointment__add')
   })
 })
