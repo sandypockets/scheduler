3e82f2341a09e3c5a9e23a1beb99518924436402
sandypockets <zac@znacreative.com>
2021-04-21 16:06:25 -0400

    Add data-testid

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Empty(props) {
 
   return (
-    <main className="appointment__add">
+    <main 
+    className="appointment__add">
       <img
+        data-testid="add"
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
