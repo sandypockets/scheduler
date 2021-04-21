8532068e9b020ae092e3b33a728a57d5f634e7c8
sandypockets <zac@znacreative.com>
2021-04-21 16:19:43 -0400

    Fix bugs introduced after resetting the DB after each test

--- cypress/integration/appointment.spec.js
+++ cypress/integration/appointment.spec.js
@@ -1,9 +1,10 @@
 describe("Booking Actions", () => {
   it("should book an interview", () => {
+    cy.request("GET", "/api/debug/reset")
     // Visits the root of our web server
     cy.visit('/')
     // Clicks on the "Add" button in the second appointment
-      .get(':nth-child(3) > .appointment__add > [data-testid=add]').click()
+      .get(':nth-child(2) > .appointment__add > [data-testid=add]').click()
       .get('[data-testid=student-name-input]')
     // Enters their name
       .type('Bob Test Name')
@@ -12,10 +13,11 @@ describe("Booking Actions", () => {
     // Clicks the save button
       .get('.button--confirm').click()
     // Sees the booked appointment
-      .get(':nth-child(3) > .appointment__card')
+      .get(':nth-child(2) > .appointment__card')
   })
 
   it("should edit an interview", () => {
+    cy.request("GET", "/api/debug/reset")
     // Visits the root of our web server
     cy.visit('/')
     // Clicks the edit button for the existing appointment
@@ -32,10 +34,11 @@ describe("Booking Actions", () => {
   })
 
   it("should cancel an interview", () => {
+    cy.request("GET", "/api/debug/reset")
     // Visits the root of our web server
     cy.visit('/')
     // Clicks the delete button for the existing appointment
-      .get(':nth-child(2) > .appointment__card')
+      .get(':nth-child(1) > .appointment__card')
       .get('[alt=Delete]').first().click({ force: true })
     // Clicks the confirm button
       .get('.appointment__card > .appointment__actions > :nth-child(2)').click()
